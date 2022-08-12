ba20c8bf93b0a0ff5c7770cc7c52e16d7fd883d1
Emre Mert <emre.mrt98@gmail.com>
2022-08-12 11:43:15 +0300

    final prev 3

--- src/components/minter/Minter.js
+++ src/components/minter/Minter.js
@@ -18,6 +18,8 @@ const Minter = (props) => {
     const { address, status } = await getCurrentWalletConnected();
     setWallet(address)
     setStatus(status);
+
+    addWalletListener();
   }, []);
 
   const connectWalletPressed = async () => {
@@ -26,9 +28,32 @@ const Minter = (props) => {
     setWallet(walletResponse.address);
   };
 
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
 
-  
   const onMintPressed = async () => { //TODO: implement
 
   };
